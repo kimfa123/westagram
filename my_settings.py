576ade63bb6b78e3f3e83341b491925471c0532f
JiHoon KIM <jihoonkim@JiHoonui-MacBookAir.local>
2021-09-26 13:09:18 +0900

    westagram setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': 'rlawlgns123!@#',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-b#85q**x1kufrx9b0w&o2#(5eg!%85r@$lf*fvhyy(p6k=*+(t'
